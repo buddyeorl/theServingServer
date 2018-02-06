b75b09d4c410f08dce39f2c38abfce8ffbf2eed1
Alexander A Lizarraga <alexander_lizarraga@ymail.com>
2018-02-06 15:54:47 -0500

    added basic functionality

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+// ===============================================================================
+// DATA
+// Below is the data holding a dummy friend data to compare
+// ===============================================================================
+
+var userData =[
+	{
+	  name:"Ahmed",
+	  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	  scores:[
+	      5,
+	      1,
+	      4,
+	      4,
+	      5,
+	      1,
+	      2,
+	      5,
+	      4,
+	      1
+	    ]
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = userData;
+
+
+
